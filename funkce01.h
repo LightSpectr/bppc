2b4192230ebe407e406d8f24610e8002fa147b67
LightSpectr <maked440@gmail.com>
2017-09-26 01:12:24 +0200

    Create funkce01.h

---
+++ funkce01.h
@@ -0,0 +1,11 @@
+#pragma once 
+#include <stdio.h> 
+
+#include <cstdio> 
+#include <iostream> 
+
+
+void tisk(int *aPocet, int *aIndex); 
+void setrid(unsigned long long *aPocet, int *aIndex); 
+
+int statistika(FILE *aFsrc);
